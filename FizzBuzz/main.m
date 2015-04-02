48d8245542b521b3d942e9fba6af65767d7d7d30
Jared <jaredtaylor@cbrc.net>
2015-04-02 10:13:17 -0400

    print "Fizz" for multiples of 3; print "Buzz" for multiples of 5; print "FizzBuzz" for multiples of 15

--- FizzBuzz/main.m
+++ FizzBuzz/main.m
@@ -10,8 +10,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int i;
+        for (i = 0; i <= 100; i++) {
+            if (i%15 == 0) {
+                NSLog(@"FizzBuzz");
+            }
+            else if (i%3 == 0){
+                NSLog(@"Fizz");
+            }
+            else if (i%5 == 0){
+                NSLog(@"Buzz");
+            }
+            else{
+                NSLog(@"%i", i);
+            }
+            
+        }
+        
+        
     }
     return 0;
 }
